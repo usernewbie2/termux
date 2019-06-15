bc1e2896953e24393f93f4409bc77300b8cf0e26
usernewbie2 <51822311+usernewbie2@users.noreply.github.com>
2019-06-15 20:18:11 +0700

    Update dreamss.sh

--- dreamss.sh
+++ dreamss.sh
@@ -11,16 +11,17 @@ ku='\033[33;1m' #kuning
 #Capek anjing buat nya
 echo
 echo
-python2 security.py
-echo
 clear
-echo
+
+
+
+
 
 echo -e  "_____________________________________________________________"
 echo -e  "Tools    : User Termux3 $white         " |lolcat
 echo -e  "Creadby  : Mr.Rmdhn $white   " |lolcat
 echo -e  "Contact  : ramahore14@gmail.com $white " |lolcat
-echo -e  "_____________________________________"
+echo -e  "_____________________________________________________________"
 
 sleep 1
 echo
