29b63cf9e39e72a50b1b6c16fb44367fb8969bc3
usernewbie2 <51822311+usernewbie2@users.noreply.github.com>
2019-06-17 16:10:13 +0700

    Update dreamss.sh

--- dreamss.sh
+++ dreamss.sh
@@ -139,6 +139,268 @@ echo $pur"==================================================="
 echo $ku"40. Vitex ( Virus Txt Wa)"
 echo $ku"===================================================="
 
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Recode? Nggk punya Otak lu!!!
+#Capek anjing buat nya
+echo
+echo
+python2 security.py
+echo
+clear
+echo
+sleep 1
+echo $cy"SUBCRIBE CHANNEL : MR.F4K3 YT"
+echo $cy"LINK CHANNEL : bit.ly/FakeYT"
+echo $cy"WA : 081260032271"
+echo $cy"PLEASE SUBSCRIBE"
+sleep 1
+echo $i"YANG MAU RECODE PM GW WA 081260032271"
+clear
+sleep 5
+echo $cy""
+echo $pu"/"$ku"Created : Jhosua"
+echo $pu"/"$ku"Noopyright"
+echo $pu"/"$ku"Program Bash"
+echo $pu"/"$ku"Type Installer"
+echo $cy""
+echo
+sleep 1
+echo $i"    /\_\     AUTHOR    : JHOSUA SAUT MARULI"
+sleep 1
+echo $i"   /././_    WA        : 081260032271"
+sleep 1
+echo $i"  /./_/\.\   GMAIL     : jhosuasautmaruli666@gmail.com"
+sleep 1
+echo $i"  \.\/..\.\  VERSION   : 1.5"
+sleep 1
+echo $i"/\.\../\.\_\ TYPE      : INSTALLER"
+sleep 1
+echo $i"\.\/..\.\/_/ UPDATE    : 23 Mei 2019"
+sleep 1
+echo $i" \../\.\_\   YOUTUBE   : Mr.F4K3 YT"
+sleep 1
+echo $i"  \/_/././   TELEGRAM  : https://t.me/mrf4k3"
+sleep 1
+echo $i"    /././    STATUS    : ACTIVE"
+sleep 1
+echo $i"    \/_/     TOOL      : 310"
+sleep 1
+echo
+echo $cy""
+echo $cy"DAFTAR TOOLS"
+echo $cy""
+echo
+sleep 1
+echo "==================================================="
+echo $i"1. Install OSIF             "
+echo $i"==================================================="
+echo $cy"2. Install MBF              "
+echo $cy"==================================================="
+echo $me"3. SpamCall     (Not WORK)  "
+echo $me"==================================================="
+echo $bi"4. Install RED_Hawk         "
+echo $bi"==================================================="
+echo $pur"5. DDOS                     "
+echo $pur"==================================================="
+echo $ku"6. Bruteforce Fb ( root )   "
+echo $ku"===================================================" 
+echo $bi"7. Yahoo Cloning Facebook"
+echo $bi"==================================================="
+echo $ku"8. Profile Guard Facebook"
+echo $ku"==================================================="
+echo $cy"9. Report Facebook Work"
+echo $cy"==================================================="
+echo $i"10. Install MetasPloit"
+echo $i"==================================================="
+echo $me"11. Buat Virus Android       "
+echo $me"==================================================="
+echo $pu"12. Install Sql Map          "
+echo $pu"==================================================="
+echo $bi"13. AUTOLIKE FACEBOOK        "
+echo $bi"==================================================="
+echo $pur"14. PHISING FACEBOOK 1       "
+echo $pur"==================================================="
+echo $cy"15. PHISING FACEBOOK 2       "
+echo $cy"==================================================="
+echo $cy"16. PHISING FACEBOOK 3       "
+echo $pu"==================================================="
+echo $i"17. PHISING FACEBOOK 4       "
+echo $i"==================================================="
+echo $cy"18. INSTALLTool  "
+echo $cy"==================================================="
+echo $me"19. Install Lazymux"
+echo $me"==================================================="
+echo $pu"20. Install LiteSpam"
+echo $pu"==================================================="
+echo $i"21. Install Malware (virus android)"
+echo $i"==================================================="
+echo $ku"22. Create Phising Game"
+echo $ku"==================================================="
+echo $pur"23. Install Script Deface Creator"
+echo $pur"==================================================="
+echo $pu"24. Install Weeman (Buat Phising)"
+echo $pu"==================================================="
+echo $cy"25. Hack CCTV"
+echo $cy"==================================================="
+echo $me"26. Webdav (Buat Deface)"
+echo $me"==================================================="
+echo $i"27. SpamCall (NEW)"
+echo $i"==================================================="
+echo $bi"28. Spam Sms"
+echo $bi"==================================================="
+echo $pur"29. Gps Tracking"
+echo $pur"==================================================="
+echo $i"30. Wifi HACKER (Root)"
+echo $i"==================================================="
+echo $ku"31. Install Hammer (DDOS)"
+echo $ku"==================================================="
+echo $cy"32. Install Tools BAJINGANv6"
+echo $cy"==================================================="
+echo $pu"33. Install MBF 4x speeed"
+echo $pu"==================================================="
+echo $me"34. Brute Force FB"
+echo $me"==================================================="
+echo $i"35. SpamFb"
+echo $i"==================================================="
+echo $bi"36. Install SOCIAL BOX"
+echo $bi"==================================================="
+echo $pu"37. Auto Followers IG"
+echo $pu"==================================================="
+echo $cy"38. Brute Force IG"
+echo $cy"==================================================="
+echo $pur"39. Http Server Phising Facebook"
+echo $pur"==================================================="
+echo $ku"40. Vitex ( Virus Txt Wa)"
+echo $ku"==================================================="
+echo $i"41. Install Trojoan"
+echo $i"==================================================="
+echo $pur"42. Hack Apk (Decompile)"
+echo $pur"==================================================="
+echo $me"43. Install A-Rat"
+echo $me"==================================================="
+echo $i"44. Spam SMS (GRAB)"
+echo $i"==================================================="
+echo $pu"45. Install RootHelper"
+echo $pu"==================================================="
+echo $bi"46. Spam Gmail"
+echo $bi"==================================================="
+echo $cy"47. Sadap Camera"
+echo $cy"==================================================="
+echo $ku"48. Tools Facebook"
+echo $ku"==================================================="
+echo $me"49. Wordlist Creator"
+echo $me"==================================================="
+echo $pur"50. Install Hunner Framework"
+echo $pur"==================================================="
+echo $i"51. Multi Brute Facebook New (NO CHECKPOINT)"
+echo $i"==================================================="
+echo $bi"52. Install Tool-X"
+echo $bi"==================================================="
+echo $pur"53. INSTAHACK"
+echo $pur"==================================================="
+echo $pu"54. Dork Scanner"
+echo $pu"==================================================="
+echo $cy"55. MD5 Cracker"
+echo $cy"==================================================="
+echo $ku"56. WP Scan"
+echo $ku"==================================================="
+echo $me"57. Proxy Checker"
+echo $me"==================================================="
+echo $i"58. Sniper - Scanner"
+echo $i"==================================================="
+echo $bi"59. Check IP"
+echo $bi"==================================================="
+echo $pu"60. Prankers"
+echo $pu"==================================================="
+echo $bi"61. Admin Login"
+echo $bi"==================================================="
+echo $pur"62. Brute Force IG"
+echo $pur"==================================================="
+echo $ku"63. IP Tracer"
+echo $ku"==================================================="
+echo $me"64. Nmap Lite"
+echo $me"==================================================="
+echo $cy"65. Trap Project"
+echo $cy"==================================================="
+echo $i"66. Termux Alice"
+echo $i"==================================================="
+echo $pu"67. Zeebsploit"
+echo $pu"==================================================="
+echo $ku"68. ReconT"
+echo $ku"==================================================="
+echo $bi"69. G-Tools"
+echo $bi"==================================================="
+echo $pur"70. Ultimate Dork"
+echo $pur"==================================================="
+echo $i"71. Penetration Revolution"
+echo $i"==================================================="
+echo $me"72. Yahoo Filter"
+echo $me"==================================================="
+echo $pu"73. XSS-Check"
+echo $pu"==================================================="
+echo $cy"74. G-Send"
+echo $cy"==================================================="
+echo $ku"75. Kahoot Hack"
+echo $ku"==================================================="
+echo $bi"76. Unfriend Facebook"
+echo $bi"==================================================="
+echo $i"77. Domain Checker"
+echo $i"==================================================="
+echo $pu"78. UnMarshalling Khusus Python"
+echo $pu"==================================================="
+echo $bi "79. Web Vuln Scan"
+echo $bi"==================================================="
+echo $cy"80. Tools DarkCurut"
+echo $cy"==================================================="
+echo $ku"81. Tools-Box-Exploiter"
+echo $ku"==================================================="
+echo $pur"82. Black Engineer"
+echo $pur"==================================================="
+echo $me"83. Descrypt Python"
+echo $me"==================================================="
+echo $pu"84. UserRecon"
+echo $pu"==================================================="
+echo $cy"85. Bot Komen"
+echo $cy"==================================================="
+echo $bi"86. Termux Api Free Sms"
+echo $bi"==================================================="
+echo $i"87. Kadal Tools"
+echo $i"==================================================="
+echo $ku"88. Mp3 Downloader"
+echo $ku"==================================================="
+echo $me"89. Install LITESCRIPT"
+echo $me"==================================================="
+echo $pur"90. Install Tools AsecC"
+echo $pur"==================================================="
+echo $cy"91. GGMux"
+echo $cy"==================================================="
+echo $pu"92. Toxic Tools"
+echo $pu"==================================================="
+echo $bi"93. Termux Add Login"
+echo $bi"==================================================="
+echo $ku"94. Black Eye"
+echo $ku"==================================================="
+echo $i"95. Free Programing Books"
+echo $i"==================================================="
+echo $me"96. BlacNmap"
+echo $me"==================================================="
+echo $cy"97. Ettercap For Android"
+echo $cy"==================================================="
+echo $pur"98. Scan IP"
+echo $pur"==================================================="
+echo $pu"99. Facebook Checker"
+echo $pu"==================================================="
+echo $ku"100. Auto Visitor"
+echo $ku"==================================================="
+
 read -p"[ SELECT NUMBER] : " pil;
 
 if [ $pil = 1 ]
@@ -544,6 +806,486 @@ cd virtex
 sh virtex.sh
 fi
 
+if [ $pil = 41 ]
+then
+clear
+git clone https://github.com/pashayogi/Trojan
+cd Trojoan
+python2 Trojoan.py
+fi
+
+if [ $pil = 42 ]
+then
+clear
+git clone https://github.com/adi1090x/hackapk
+cd hackapk
+bash hackapk
+fi
+
+if [ $pil = 43 ]
+then
+clear
+git clone https://github.com/Xi4u7/A-Rat.git
+cd A-Rat
+python2 A-Rat.py
+fi
+
+if [ $pil = 44 ]
+then
+clear
+git clone https://github.com/GUNAWAN18ID/spammer-sms
+cd spammer-sms
+python2 spam.py
+fi
+
+if [ $pil = 45 ]
+then
+clear
+git clone https://github.com/NullArray/RootHelper
+cd RootHelper
+sh roothelper.sh
+fi
+
+if [ $pil = 46 ]
+then
+clear
+git clone https://github.com/revan-ar/mail-spammer
+cd mail-spammer
+php mail.php
+fi
+
+if [ $pil = 47 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/saycheese
+cd saycheese
+bash saycheese.sh
+fi
+
+if [ $pil = 48 ]
+then
+clear
+git clone https://github.com/LOoLzeC/F-HUNTER
+cd F-HUNTER
+python2 fhunter.py
+fi
+
+if [ $pil = 49 ]
+then
+clear
+git clone https://github.com/Cabdulahi/wordlist
+cd wordlist
+python2 word.py
+fi
+
+if [ $pil = 50 ]
+then
+clear
+git clone https://github.com/b3-v3r/Hunner.git
+cd Hunner
+chmod +x hunner.py
+python2 hunner.py
+fi
+
+if [ $pil = 51 ]
+then
+clear
+git clone https://github.com/Jhosua00/multibrutefb
+cd multibrutefb
+python2 lucifer.py
+fi
+
+if [ $pil = 52 ]
+then
+clear
+git clone https://github.com/Rajkumrdusad/Tool-X
+cd Tool-X
+chmod 777 install.aex
+./install.aex
+python2 Tool-X.py
+fi
+
+if [ $pil = 53 ]
+then
+clear
+git clone https://github.com/KALSELeXploit/INSTAHACK
+cd INSTAHACK
+python2 insta.py
+fi
+
+if [ $pil = 54 ]
+then
+clear
+git clone https://github.com/KALSELeXploit/DORK-SCANNER
+cd DORK-SCANNER
+php scan.php
+fi
+
+if [ $pil = 55 ]
+then
+clear
+git clone https://github.com/KALSELeXploit/MD5-CRACKER
+cd MD5-CRACKER
+python2 md5.py
+fi
+
+if [ $pil = 56 ]
+then
+clear
+git clone https://github.com/KALSELeXploit/Wp-Scan
+cd WP-Scan
+python auto.py
+fi
+
+if [ $pil = 57 ]
+then
+clear
+git clone https://github.com/KALSELeXploit/PROXY-CHECKER
+cd PROXY-CHECKER
+python2 proxy.py
+fi
+
+if [ $pil = 58 ]
+then
+clear
+git clone https://github.com/KALSELeXploit/Sniper-Scanner
+cd Sniper-Scanner
+python kalsel.py
+fi
+
+if [ $pil = 59 ]
+then
+clear
+git clone https://github.com/KALSELeXploit/CHECK-IP
+cd CHECK-IP
+python2 checkip.py
+fi
+
+if [ $pil = 60 ]
+then
+clear
+git clone https://github.com/ClayHackerTeam/Prankers
+cd Prankers
+sh Prankers.sh
+fi
+
+if [ $pil = 61 ]
+then
+clear
+git clone https://github.com/GUNAWAN18ID/admin-login
+cd admin-login
+python2 admin-login.py
+fi
+
+if [ $pil = 62 ]
+then
+clear
+git clone https://github.com/GUNAWAN18ID/hack-akun-instagram
+cd hack-akun-instagram
+python2 wordlist.py
+fi
+
+if [ $pil = 63 ]
+then
+clear
+git clone https://github.com/Rajkumrdusad/IP-Tracer
+cd IP-Tracer
+php IP-Tracer.php
+fi
+
+if [ $pil = 64 ]
+then
+clear
+git clone https://github.com/zlucifer/nmap_lite
+cd nmap_lite
+sh nmap.sh
+fi
+
+if [ $pil = 65 ]
+then
+clear
+git clone https://github.com/zlucifer/trap_project
+cd trap_project
+sh trap.sh
+fi
+
+if [ $pil = 66 ]
+then
+clear
+git clone https://github.com/zlucifer/termux_alice
+cd termux_alice
+sh alice.sh
+fi
+
+if [ $pil = 67 ]
+then
+clear
+git clone https://github.com/LOoLzeC/Zeebsploit
+cd Zeebsploit
+python2 zsf
+fi
+
+if [ $pil = 68 ]
+then
+clear
+git clone https://github.com/jaxBCD/ReconT
+cd ReconT
+python2 reconT.py
+fi
+
+if [ $pil = 69 ]
+then
+clear
+git clone https://github.com/jaxBCD/G-Tools
+cd G-Tools
+python2 G-Tool.py
+fi
+
+if [ $pil = 70 ]
+then
+clear
+git clone https://github.com/jaxBCD/Ultimate-Dork
+cd Ultimate-Dork
+python2 ultimate-dork.py
+fi
+
+if [ $pil = 71 ]
+then
+clear
+git clone https://github.com/mrcakil/Penetration-Revolution
+cd Penetration-Revolution
+bash penrev
+fi
+
+if [ $pil = 72 ]
+then
+clear
+git clone https://github.com/Syhrularv/yahoofilter
+cd yahoofilter
+sh yahoo.sh
+fi
+
+if [ $pil = 73 ]
+then
+clear
+git clone https://github.com/KALSELeXploit/XSS-check
+cd XSS-check
+python2 xss.py
+fi
+
+if [ $pil = 74 ]
+then
+clear
+git clone https://github.com/Syhrularv/G-send
+cd G-send
+python2 gsend.py
+fi
+
+if [ $pil = 75 ]
+then
+clear
+git clone https://github.com/unixpickle/kahoot-hack
+cd kahoot-hack
+sh kh-tmux-setup.sh
+fi
+
+if [ $pil = 76 ]
+then
+clear
+git clone https://github.com/GUNAWAN18ID/unfriend-facebook
+cd unfriend-facebook
+php delete.php
+fi
+
+if [ $pil = 77 ]
+then
+clear
+git clone https://github.com/GUNAWAN18ID/Domain-Checker
+cd Domain-Checker
+python2 Domain-Checker.py
+fi
+
+if [ $pil = 78 ]
+then
+clear
+git clone https://github.com/GUNAWAN18ID/unmarshalling
+cd unmarshalling
+cd unmarshalling
+fi
+
+if [ $pil = 79 ]
+then
+clear
+git clone https://github.com/Xi4u7/webvulnscan
+cd webvulnscan
+python2 main.py
+fi
+
+if [ $pil = 80 ]
+then
+clear
+git clone https://github.com/DarkCurut08/DarkCurut08
+cd DarkCurut08
+sh Moreno77.sh
+fi
+
+if [ $pil = 81 ]
+then
+clear
+git clone https://github.com/mrcakil/ToolsBox-Exploiter
+cd ToolBox-Exploiter
+php ToolsBox.php
+fi
+
+if [ $pil = 82 ]
+then
+clear
+git clone https://github.com/LOoLzeC/BlackEngineer
+cd BlackEngineer
+python2 BlackEngineer.py
+fi
+
+if [ $pil = 83 ]
+then
+clear
+git clone https://github.com/Syhrularv/pycompiler
+cd pycompiler
+sh compile.sh
+fi
+
+if [ $pil = 84 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/userrecon
+cd userrecon
+sh userrecon.sh
+fi
+
+if [ $pil = 85 ]
+then
+clear
+git clone https://github.com/revan-ar/bot-komen
+cd bot-komen
+php bot.php
+fi
+
+if [ $pil = 86 ]
+then
+clear
+git clone https://github.com/LOoLzeC/termux-api-free-sms
+cd python2 free_sms.py
+fi
+
+if [ $pil = 87 ]
+then
+clear
+git clone https://github.com/vladimir-bayezid/Kadal-Tools
+cd Kadal-Tools
+sh kadal.sh
+fi
+
+if [ $pil = 88 ]
+then
+clear
+git clone https://github.com/vladimir-bayezid/mp3download-V1
+cd mp3download-V1
+sh mp3player.sh
+fi
+
+if [ $pil = 89 ]
+then
+clear
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+
+if [ $pil = 90 ]
+then
+clear
+git clone https://github.com/muhammadfathul/Tools-AsecC
+cd Tools-AsecC
+sh Tools-AsecC.sh
+fi
+
+if [ $pil = 91 ]
+then
+clear
+git clone https://github.com/muhammadfathul/GGMUX
+cd GGMUX
+sh ggmux.sh
+fi
+
+if [ $pil = 92 ]
+then
+clear
+git clone https://github.com/muhammadfathul/TOXIC-TOOLS
+cd TOXIC-TOOLS
+sh toxic.sh
+fi
+
+if [ $pil = 93 ]
+then
+clear
+git clone https://github.com/muhammadfathul/Termux-Add-Login
+cd Termux-Add-Login
+sh TAL.sh
+fi
+
+if [ $pil = 94 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/blackeye
+cd blackeye
+sh blackeye.sh
+fi
+
+if [ $pil = 95 ]
+then
+clear
+git clone https://github.com/AlexisAhmed/free-programming-books
+cd free-programming-books
+fi
+
+if [ $pil = 96 ]
+then
+clear
+git clone https://github.com/Xi4u7/BlackNmap
+cd BlackNmap
+bash blacknmap
+fi
+
+if [ $pil = 97 ]
+then
+clear
+git clone https://github.com/Xi4u7/EttercapForAndroid
+cd EttercapForAndroid
+bash install.sh
+fi
+
+if [ $pil = 98 ]
+then
+clear
+git clone https://github.com/ClayHackerTeam/ScanIP
+cd ScanIP
+sh ScanIP.sh
+fi
+
+if [ $pil = 99 ]
+then
+clear
+git clone https://github.com/fdciabdul/fbchecker
+cd fbchecker
+php fbcheck.php
+fi
+
+if [ $pil = 100 ]
+then
+clear
+git clone https://github.com/fdciabdul/autovisitor
+cd autovisitor
+php visit.php
+fi
 
 
 if [ $pil = 00 ]
